ea5070aa60354be79c4e261bcd276b85fc195e06
Elizabeth Emig <epitman523@gmail.com>
2019-04-15 17:08:38 -0500

    wrote out steps for needed functions

---
+++ index.js
@@ -0,0 +1,12 @@
+//Entering items:
+//1. Prevent default submission
+//2. Get value of #shopping-list-entry and store as var
+//3. Append value to .shopping-list upon submission
+
+//Checking function
+//4. Add event listener to .shopping-item-toggle
+//5. On click, toggle class .shopping-item__checked
+
+//Remove function
+//6. Add click event listener to .shopping-item-delete
+//7. On click remove li from list
