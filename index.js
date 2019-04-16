081d49a02089064474a9d10db93938e6ba3dea5d
Elizabeth Emig <epitman523@gmail.com>
2019-04-15 21:44:28 -0500

    completed check item function

--- index.js
+++ index.js
@@ -11,14 +11,18 @@
 //6. Add click event listener to .shopping-item-delete
 //7. On click remove li from list
 function shoppingList() {
-  $("#js-shopping-list-form").on("submit", function(event) {
+  $("#js-shopping-list-form").on("submit", function (event) {
     event.preventDefault();
     const newItem = $("#shopping-list-entry").val();
     $(".shopping-list").append(
       '<li><span class="shopping-item">' +
-        newItem +
-        '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
+      newItem +
+      '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
     );
   });
+  $(".shopping-list").on("click", ".shopping-item-toggle", function (event) {
+    $(this).parents('li').find('.shopping-item').toggleClass("shopping-item__checked");
+  });
+
 }
 $(shoppingList());
